25e260bb7df914e6ee9c30f05d275ef7c71e0ca0
sayanmoulick <moulick.sayan@gmail.com>
2020-08-29 22:20:22 +0530

    Todo app

--- README.md
+++ README.md
@@ -1,1 +1,41 @@
-# mean-stack-repo
+# mean-stack-repo project setup
+
+### Install Node
+
+> sudo apt install curl
+
+> curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+
+> sudo apt-get install -y nodejs
+
+> node -v
+
+v10.22.0
+
+> npm -v
+
+6.14.6
+
+### Install angular
+> npm install -g @angular/cli
+
+> ng --version
+
+* Angular CLI: 10.0.8
+* Node: 10.22.0
+* OS: linux x64
+
+### Create an application or a project
+> ng new crud-app
+
+> cd crud-app/
+
+> ng generate class Todo --skipTests=false
+
+* To verify whether our code works as expected, we can now run:
+    > ng test
+
+> ng generate service TodoData
+
+#### Run application in dev mode
+> ng serve
---
+++ crud-app/src/app/app.component.spec.ts
@@ -0,0 +1,52 @@
+import { TestBed, async } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AppComponent } from './app.component';
+import { Todo } from './todo';
+import { FormsModule } from '@angular/forms';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        FormsModule
+      ],
+      declarations: [
+        AppComponent
+      ],
+    }).compileComponents();
+  }));
+
+  it('should create the app', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app).toBeTruthy();
+  });
+
+  it(`should have as title 'crud-app'`, () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+    expect(app.title).toEqual('crud-app');
+  });
+  
+  it(`should have a newTodo todo`, async(() => {
+    let fixture = TestBed.createComponent(AppComponent);
+    let app = fixture.debugElement.componentInstance;
+    expect(app.newTodo instanceof Todo).toBeTruthy()
+  }));
+
+  it('should display "Todos" in h1 tag', async(() => {
+    let fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    let compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('Todos');
+  }));
+
+  /*it('should render title', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.nativeElement;
+    expect(compiled.querySelector('.content span').textContent).toContain('crud-app app is running!');
+  });*/
+
+});
